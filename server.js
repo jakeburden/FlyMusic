2d1a3acae61fa99987c3b1280fa25adb789bf4a0
jekrb <Jacob.JW.Burden@gmail.com>
2016-05-17 10:52:30 -0400

    remove IIFE

--- server.js
+++ server.js
@@ -1,105 +1,99 @@
-(function() {
+// include dependencies
+const express = require('express');
+const httpFactory = require('http');
+const ioFactory = require('socket.io');
+const randomColor = require('randomcolor');
 
-	'use strict';
+// initialize express and socket servers
+const app = express();
+const http = httpFactory.Server(app);
+const io = ioFactory(http);
 
-	// include dependencies
-	const express = require('express');
-	const httpFactory = require('http');
-	const ioFactory = require('socket.io');
-	const randomColor = require('randomcolor');
+// get samples module
+const samples = require('./libs/samples');
 
-	// initialize express and socket servers
-	const app = express();
-	const http = httpFactory.Server(app);
-	const io = ioFactory(http);
+var userCount = [
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false
+];
 
-	// get samples module
-	const samples = require('./libs/samples');
+// store master client id when connected
+var master = false;
 
-	var userCount = [
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false
-	];
+// set public directory as document root
+app.use(express.static('public'));
 
-	// store master client id when connected
-	var master = false;
+// socket connection initialized
+io.on('connection', (socket) => {
+	console.log('a user connected');
 
-	// set public directory as document root
-	app.use(express.static('public'));
+	// no master client specified so set now
+	if (!master) {
+		master = socket.id;
+		socket.emit('setMaster');
+		console.log('master connected');
 
-	// socket connection initialized
-	io.on('connection', (socket) => {
-		console.log('a user connected');
+	// otherwise set as client with color and sample
+	} else {
+		let sample = samples.getRandom();
+		let id = 0;
+		console.log(sample);
 
-		// no master client specified so set now
-		if (!master) {
-			master = socket.id;
-			socket.emit('setMaster');
-			console.log('master connected');
-
-		// otherwise set as client with color and sample
-		} else {
-			let sample = samples.getRandom();
-			let id = 0;
-			console.log(sample);
-
-			userCount.every(function(count, index) {
-				if (!count) {
-					id = index;
-					userCount[index] = true;
-					return false;
-				}
-
-				return true;
-			});
-
-			socket.emit('setClient', {
-				id: id,
-				color: randomColor(),
-				sample: sample
-			});
-		}
-
-		// listen for hit events from client
-		socket.on('hit', (data) => {
-			console.log('hit handler');
-			console.log(master);
-
-			// broadcast playSound event to master client with color data
-			socket.broadcast.to(master).emit('playSound', data);
-		});
-
-		socket.on('cutHit', (data) => {
-			console.log('cutHit handler');
-			console.log(master);
-
-			// broadcast playSound event to master client with color data
-			socket.broadcast.to(master).emit('stopSound', data);
-		});
-
-		// socket disconnected
-		socket.on('disconnect', () => {
-			console.log('user disconnected');
-
-			// if client was master then reset master
-			if (socket.id === master) {
-				master = false;
-				console.log('master disconnected');
-				console.log(master);
+		userCount.every(function(count, index) {
+			if (!count) {
+				id = index;
+				userCount[index] = true;
+				return false;
 			}
+
+			return true;
 		});
+
+		socket.emit('setClient', {
+			id: id,
+			color: randomColor(),
+			sample: sample
+		});
+	}
+
+	// listen for hit events from client
+	socket.on('hit', (data) => {
+		console.log('hit handler');
+		console.log(master);
+
+		// broadcast playSound event to master client with color data
+		socket.broadcast.to(master).emit('playSound', data);
 	});
 
-	// listen on port 3000
-	http.listen(3000, () => {
-		console.log('listening on *:3000');
+	socket.on('cutHit', (data) => {
+		console.log('cutHit handler');
+		console.log(master);
+
+		// broadcast playSound event to master client with color data
+		socket.broadcast.to(master).emit('stopSound', data);
 	});
 
-})();
+	// socket disconnected
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+
+		// if client was master then reset master
+		if (socket.id === master) {
+			master = false;
+			console.log('master disconnected');
+			console.log(master);
+		}
+	});
+});
+
+// listen on port 3000
+http.listen(3000, () => {
+	console.log('listening on *:3000');
+});
